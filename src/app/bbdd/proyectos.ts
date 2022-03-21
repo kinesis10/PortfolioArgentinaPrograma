5fd85d47ba1daa82e6179f563d7d709d90761beb
Chesini <pchesin1@gmail.com>
2022-03-21 17:02:50 -0300

    Aguegue una lista de proyectos simulando una BBDD

---
+++ src/app/bbdd/proyectos.ts
@@ -0,0 +1,21 @@
+import { Proyecto } from "./proyecto" //importo la interface
+
+export const PROYECTOS: Proyecto[]= [ //exporto el arreglo de tipo objeto
+    {
+        id: 1,
+        nombre: "titulo proyecto 1",
+        texto: "Descripcion del proyecto",
+    },
+    {
+        id: 2,
+        nombre: "titulo proyecto 2",
+        texto: "Descripcion del proyecto",
+    },
+
+{
+    id: 3,
+    nombre: "titulo proyecto 3",
+    texto: "Descripcion del proyecto",
+},
+
+]
